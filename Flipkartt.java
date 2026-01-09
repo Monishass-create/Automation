3b17048582b5ab40f8b94bf58208f670b9372777
Monishass-create <myselfselenophile61@gmail.com>
2026-01-09 22:04:20 +0530

    Add files via upload

---
+++ Flipkartt.java
@@ -0,0 +1,37 @@
+package autosuggestions;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipkart
+{
+	public static void main(String[] args)
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		//navigate to flipkart
+		driver.get("https://www.flipkart.com/");
+		
+		//locate the stf
+		driver.findElement(By.name("q")).sendKeys("puma");
+		
+		//capture the autosuggestions
+		List<WebElement> ele = driver.findElements(By.xpath("//li[@class='Sc1DCn']"));
+		
+		for (WebElement web: ele) 
+		{
+			System.out.println(web.getText());
+			
+		}
+		
+	}
+
+}
